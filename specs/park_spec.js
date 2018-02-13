e8ca07f02c1eaffd4dab576b3cad0f6d86dee566
DocLJN <ljnoble@gmail.com>
2018-02-13 22:41:30 +0000

    Failing test for add dinosaur

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,17 +1,26 @@
 const Park = require("../park");
 const assert = require("assert");
+const Dinosaur = require("../dinosaur");
 
 let park;
+let triceratops;
 
 describe("Park", function () {
 
   beforeEach(function () {
     park = new Park ();
+    triceratops = new Dinosaur ( "Triceratops", 10 ) ;
   });
 
   it("should start with an empty enclosure", function () {
     actual = park.getDinosaurCount();
     assert.strictEqual(actual, 0);
+  });
+
+  it("should be able to add a dinosaur", function () {
+    park.addDinosaur(triceratops);
+    actual = park.getDinosaurCount;
+    assert.strictEqual(actual, 1);
   });
 
 });
