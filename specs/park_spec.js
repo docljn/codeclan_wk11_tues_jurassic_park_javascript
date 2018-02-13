af6d336fb93e1e567db1e4c34000e9812164a4d1
DocLJN <ljnoble@gmail.com>
2018-02-13 22:52:43 +0000

    Failing test for remove all of type

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -3,13 +3,29 @@ const assert = require("assert");
 const Dinosaur = require("../dinosaur");
 
 let park;
+let park_full;
 let triceratops;
+let velociraptor;
+let velociraptor_2;
+let tRex;
+let tRex_2;
+
 
 describe("Park", function () {
 
   beforeEach(function () {
     park = new Park ();
     triceratops = new Dinosaur ("Triceratops", 10) ;
+    velociraptor = new Dinosaur ("Velociraptor", 20) ;
+    tRex = new Dinosaur ("TRex", 1) ;
+    velociraptor_2 = new Dinosaur ("Velociraptor", 20) ;
+    tRex_2 = new Dinosaur ("TRex", 1) ;
+    park_full = new Park ();
+    park_full.addDinosaur(triceratops);
+    park_full.addDinosaur(velociraptor);
+    park_full.addDinosaur(tRex);
+    park_full.addDinosaur(velociraptor_2);
+    park_full.addDinosaur(tRex_2);
   });
 
   it("should start with an empty enclosure", function () {
@@ -21,6 +37,12 @@ describe("Park", function () {
     park.addDinosaur(triceratops);
     actual = park.getDinosaurCount();
     assert.strictEqual(actual, 1);
+  });
+
+  it("should be able to remove all dinosaurs of a particular type", function () {
+    park_full.removeAllOfType("tRex");
+    actual = park_full.getDinosaurCount();
+    assert.strictEqual(actual, 3);
   });
 
 });
