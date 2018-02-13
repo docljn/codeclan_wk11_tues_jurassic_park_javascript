edeebbc47b8c6abb667f989043a651cdb143e8b4
DocLJN <ljnoble@gmail.com>
2018-02-13 22:58:48 +0000

    Passing test for remove all of type

--- park.js
+++ park.js
@@ -10,6 +10,15 @@ Park.prototype.addDinosaur = function (dinosaur) {
   this.enclosure.push(dinosaur);
 };
 
+Park.prototype.removeAllOfType = function (type) {
+  for (dinosaur of this.enclosure) {
+    if (dinosaur.type.toLowerCase() == type.toLowerCase()) {
+      const index_to_remove = this.enclosure.indexOf(dinosaur);
+      this.enclosure.splice(index_to_remove, 1);
+    }
+  }
+};
+
 
 
 module.exports = Park;
